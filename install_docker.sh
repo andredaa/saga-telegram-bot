734c98f482a0cef7953cb95180922d2a7d9f2a06
andredaa <post.for.andre@gmail.com>
2021-05-13 16:01:09 +0200

    Always restart docker

--- install_docker.sh
+++ install_docker.sh
@@ -3,5 +3,5 @@
 docker stop sagabot_i
 docker rm sagabot_i
 docker build -t=sagabot ./
-docker run -d --name sagabot_i sagabot
+docker run -d --name sagabot_i sagabot --restart=always
 docker logs -f sagabot_i
